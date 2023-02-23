7b686fe29784d71b5a30c841f21df09c5f95a824
krasi <krasimir.xristov@gmail.com>
2023-02-23 14:48:43 +0100

    create pages and routes

--- motor-vehicles-world/src/App.js
+++ motor-vehicles-world/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="text-3xl bg-red-600">Hello from MVW</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/sing-in" element={<SignIn/>} />
+      <Route path="/sing-up" element={<SignUp/>} />
+      <Route path="/forgot-password" element={<ForgotPassword/>} />
+      <Route path="/offers" element={<Offers/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
