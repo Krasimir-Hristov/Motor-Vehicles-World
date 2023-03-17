78c47a7038a4afff0db8be1154d3fc3d3d6735ed
krasi <krasimir.xristov@gmail.com>
2023-03-17 21:09:59 +0100

    Starting create funcionality in create listing page

--- motor-vehicles-world/src/App.js
+++ motor-vehicles-world/src/App.js
@@ -18,15 +18,25 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={ <Home /> } />
+
           <Route path='/profile' element={ <PrivetRoute />}> 
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={ <Profile /> } />
           </Route>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
+
+          <Route path="/sign-in" element={ <SignIn /> } />
+
+          <Route path="/sign-up" element={ <SignUp /> } />
+
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+
+          <Route path="/offers" element={ <Offers />} />
+          
+          <Route path='/create-listing' element={ <PrivetRoute/> }>
+          <Route path="/create-listing" element={ <CreateListing /> } />
+          </Route> 
+            
+          
         </Routes>
       </Router>
       <ToastContainer
