ac00f6b2d57ba0cbfa38c36e072d7c5e06386175
krasi <krasimir.xristov@gmail.com>
2023-03-24 10:25:24 +0100

    Add share copy funcionality

--- motor-vehicles-world/src/pages/Listing.jsx
+++ motor-vehicles-world/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { doc, getDoc } from 'firebase/firestore';
@@ -7,6 +7,7 @@ import { db } from '../firebase.config';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/css/bundle';
+import { FaShare } from 'react-icons/fa';
 
 import  Spinner  from '../components/Spinner';
 
@@ -15,6 +16,8 @@ export default function Listing() {
     const params = useParams();
     const [ listing, setListing ] = useState(null);
     const [ loading, setLoading ] = useState(true);
+    const [ shareLinkCopied, setShareLinkCopied ] = useState(false);
+
     SwiperCore.use([Autoplay, Navigation, Pagination]);
 
     useEffect(() => {
@@ -57,6 +60,25 @@ export default function Listing() {
                 </SwiperSlide>
             )) }
         </Swiper>
+
+        <div className="fixed top-[13%] right-[3%] z-10 bg-white 
+        cursor-pointer text-black border-2 border-gray-400 rounded-full w-12 h-12 flex 
+        justify-center items-center"
+         onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setShareLinkCopied(true);
+            setTimeout(()=> {
+                setShareLinkCopied(false);
+            }, 2000);
+         }}
+        >
+                <FaShare className='text-lg text-slate-700'/>
+        </div>
+        {shareLinkCopied && (
+            <p className='fixed top-[23%] right-[5%] font-semibold border-spacing-2 border-gray-400 
+            rounded-md bg-white z-10 text-black p-2'
+            >Link Copied</p>
+        )}
          </main>);
 
 }
